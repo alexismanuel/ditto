36ad62b2a47af8b97fcd36336eb8604427493698
Alexis Manuel <alexis.manuelpro@gmail.com>
2024-07-03 10:44:17 +0200

    chore: remove obsolete scripts

--- scripts/code.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-poetry run black ditto
-poetry run isort ditto
-poetry run yapf -i -r ditto
--- scripts/test.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-poetry run python -m pytest --cov=ditto --cov-report term-missing tests
