5f355592eea6880b08da4dbd6b6d2fa3d9c77022
Alexis Manuel <alexis.manuelpro@gmail.com>
2024-07-08 10:40:10 +0200

    feat: add register and serve functions

--- ditto/core.py
+++ ditto/core.py
@@ -1,4 +1,25 @@
 from __future__ import annotations
+import inspect
+from functools import wraps
+
+
+def inject(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        sig = inspect.signature(func)
+        for name, param in sig.parameters.items():
+            if name not in kwargs and param.default == inspect.Parameter.empty:
+                kwargs[name] = ServiceRegistry.get_instance().get(name)
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+def register(service):
+    registry = ServiceRegistry.get_instance()
+    service_class = service if inspect.isclass(service) else service.__class__
+    base_class = service_class.__bases__.get(0, service_class)
+    registry.register(base_class, service)
 
 
 class ServiceRegistry:
@@ -22,3 +43,4 @@ class ServiceRegistry:
         if cls.instance is None:
             cls.instance = cls()
         return cls.instance
+
